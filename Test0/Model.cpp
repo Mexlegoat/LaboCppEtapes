e8ceb1daceb8643076ba4eb8b3e3a03ccb0bffef
Issam <issam.melloukifilali@student.hepl.be>
2025-09-19 21:00:48 +0200

    Initial deposit

---
+++ Test0/Model.cpp
@@ -0,0 +1,18 @@
+#include "Model.h"
+// Constructeurs
+Model::Model()
+{
+	power = 0;
+	basePrice = 0;
+}
+
+
+
+
+
+// Autres Fonctions
+void Model::display()
+{
+	cout << "Puissance: " << power << " Prix de base :" << basePrice << endl;
+
+} 
---
+++ Test0/Model.h
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class Model
+{
+private:
+  int power;
+  float basePrice;
+
+public:
+  Model();
+
+  void display();
+};
