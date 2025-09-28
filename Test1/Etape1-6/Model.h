179b02a5354c8ddc2956cf153c320b4c96144938
Issam <issam.melloukifilali@student.hepl.be>
2025-09-28 19:58:54 +0200

    Test1 validé!

---
+++ Test1/Etape1-6/Model.h
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+enum Engine { Petrol, Diesel, Electric, Hybrid };
+
+class Model
+{
+private:
+  int power;
+  float basePrice;
+  char* name;
+  Engine engine;
+
+public:
+  Model();
+  ~Model();
+  Model(const char *n, int power, Engine e, float prix);
+  Model (const Model&);
+  void display();
+  void setName(const char* modelName);
+  void setPower(int power);
+  void setEngine(Engine engine);
+  void setBasePrice(float basePrice);
+  const char* getName();
+  int getPower();
+  Engine getEngine();
+  float getBasePrice();
+};
