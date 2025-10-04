a09ea5bd8d1e6050b78b4bfa36e6d5b7859b8de5
Issam <issam.melloukifilali@student.hepl.be>
2025-10-04 16:16:47 +0200

    Test2a fini

---
+++ Test2a/Etape3/Car.cpp
@@ -0,0 +1,50 @@
+#include <cstring>
+#include "Car.h"
+// Constructeurs
+
+Car::Car()
+{
+	name = nullptr;
+	model = Model();
+}
+Car::~Car()
+{
+	cout << "Destructeur" << endl;
+	if (name != nullptr)
+	{
+		delete[] name;
+	}
+}
+Car::Car(const char* nom, Model m)
+{
+	name = new char[strlen(nom) + 1];
+	strcpy(name, nom);
+	model = m;
+}
+// Autres fonctions
+// display
+void Car::display()
+{
+	cout << name << endl;
+	model.display();
+}
+// setters
+void Car::setName(const char* copy)
+{
+	name = new char[strlen(copy) + 1];
+	strcpy(name, copy);
+}
+
+void Car::setModel(Model m)
+{
+	model = m;
+}
+// getters
+Model Car::getModel()
+{
+	return model;
+}
+char* Car::getName()
+{
+	return name;
+}
---
+++ Test2a/Etape4/Car.h
@@ -0,0 +1,24 @@
+#ifndef CAR_H
+#define CAR_H
+#include <iostream>
+#include <string>
+#include "Model.h"
+using namespace std;
+
+class Car
+{
+private:
+	char* name;
+	Model model;
+
+public:
+	Car();
+	Car(const char* nom, Model m);
+	~Car();
+	void display();
+	void setName(const char* n);
+	void setModel(Model m);
+	Model getModel();
+	char* getName();
+};
+#endif
