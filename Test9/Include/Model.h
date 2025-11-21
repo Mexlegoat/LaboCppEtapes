f7bcea137c5d6ef01c0a8fe71534b5e797f0c3e0
Issam <issam.melloukifilali@student.hepl.be>
2025-11-21 22:56:54 +0100

    Test9: Quelques changements dans Model.cpp, Car.cpp pour que le programme ne crash pas Ajout de 4 fonction dans applicgaragewindow.cpp À revoir: Essayer de résoudre le probleme de Supprimer une option qui n'existe pas (crash pour le moment)

---
+++ Test9/Include/Model.h
@@ -0,0 +1,48 @@
+#ifndef MODEL_H
+#define MODEL_H
+#include <iostream>
+#include <cstring>
+using namespace std;
+namespace carconfig
+{
+  enum Engine { Petrol, Diesel, Electric, Hybrid };
+
+  class Model
+  {
+  private:
+    int power;
+    float basePrice;
+    char* name;
+    Engine engine;
+    string image;
+  public:
+    Model();
+    Model(const char *n, int power, Engine e, float prix);
+    Model(const Model& m);
+    ~Model();
+    friend istream& operator>>(istream& s, Model& m);
+    friend ostream& operator<<(ostream& s, const Model& m);
+
+    void display() const;
+
+    void setPower(int);
+    void setBasePrice(float);
+    void setName(const char*);
+    void setEngine(Engine e);
+    void setEngine(int e);
+    void setImage(const string& i);
+
+
+    string enginetoString(Engine e) const;
+    string toString() const;
+
+
+    float getBasePrice() const;
+    const char* getName() const;
+    int getPower() const;
+    Engine getEngine() const;
+    string getImage() const;
+  };
+}
+
+#endif
